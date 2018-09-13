d6f931178527757bca12ac1a3ce3e10beaba2677
jtang93 <tang.jonathan93@gmail.com>
2018-09-13 06:58:36 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
-def my_each(collection) # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
   i = 0
-  while i < collection.length
-    yield collection[i]
+  while i < array.length
+    yield array[i]
     i += 1
   end
 end
 
-my_each() do |i|
+my_each(array) do |i|
   puts i
 end
